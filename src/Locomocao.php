2e40f75688091c15348a5c809e2e754a9e3968e3
Pedro H C Miguez <pcostamiguez@gmail.com>
2024-08-25 21:09:02 -0300

    Construtor simplificado

--- src/Locomocao.php
+++ src/Locomocao.php
@@ -3,14 +3,8 @@ declare(strict_types=1);
 
 namespace Gasolina;
 
-class Locomocao
+readonly class Locomocao
 {
-    private float $distanciaCasaTrabalhoKm;
-    private float $valorLitroGasolina;
-    private float $kmPercorridoLitroGasolina;
-    private int $diasTrabalhados;
-    private float $taxaErro;
-
     /**
      * @param float $distanciaCasaTrabalhoKm
      * @param float $valorLitroGasolina
@@ -18,17 +12,12 @@ class Locomocao
      * @param int $diasTrabalhados
      * @param float $taxaErro
      */
-    public function __construct(float $distanciaCasaTrabalhoKm,
-                                float $valorLitroGasolina,
-                                float $kmPercorridoLitroGasolina,
-                                int $diasTrabalhados = 22,
-                                float $taxaErro = 1.1)
+    public function __construct(private float $distanciaCasaTrabalhoKm,
+                                private float $valorLitroGasolina,
+                                private float $kmPercorridoLitroGasolina,
+                                private int   $diasTrabalhados = 22,
+                                private float $taxaErro = 1.1)
     {
-        $this->distanciaCasaTrabalhoKm = $distanciaCasaTrabalhoKm;
-        $this->valorLitroGasolina = $valorLitroGasolina;
-        $this->kmPercorridoLitroGasolina = $kmPercorridoLitroGasolina;
-        $this->diasTrabalhados = $diasTrabalhados;
-        $this->taxaErro = $taxaErro;
     }
 
     public function getValorGastoDiario(): float
@@ -44,6 +33,6 @@ class Locomocao
     public function getTextoTotalFormatado(): string
     {
         $valorFormatado = number_format($this->getValorGastoTotal(), 2);
-        return "Gasto total por {$this->diasTrabalhados} dias trabalhados: R\${$valorFormatado} reais aproximadamente.";
+        return "Gasto total por $this->diasTrabalhados dias trabalhados: R\$$valorFormatado reais aproximadamente.";
     }
 }
